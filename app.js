2c742603123e8d63b8e6ae9afb430aabbc171601
Xama <bdguild82@gmail.com>
2026-01-21 18:24:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+const API = "PASTE_YOUR_GAS_URL";
+const market = document.getElementById("marketGrid");
+
+fetch(API)
+.then(r => r.json())
+.then(data => {
+  market.innerHTML = "";
+  data.forEach(b => {
+    market.innerHTML += `
+      <div class="card">
+        <div class="brainrot">
+          <img src="${b.img}">
+        </div>
+        <div class="name">${b.name}</div>
+        <div class="prices">
+          <button>${b.robux}</button>
+          <button>${b.money}</button>
+        </div>
+      </div>
+    `;
+  });
+});
 
